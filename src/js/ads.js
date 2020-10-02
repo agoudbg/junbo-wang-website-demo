3b96b6b33e551a6d079f52e40e57598e9d399892
agou <2833769658@qq.com>
2020-10-02 17:03:43 +0800

    -

---
+++ src/js/ads.js
@@ -0,0 +1,32 @@
+ad_types=[/* 正方形 */"7888411968", /* 横向 */"8287867219", ];
+function showad(follow, divid, type){
+	
+	document.cookie="username=John Doe; expires=Thu, 18 Dec 2043 12:00:00 GMT; path=/";
+	
+	
+	
+	
+}
+// <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+// <!-- 通用横向展示广告 -->
+// <ins class="adsbygoogle"
+     // style="display:block"
+     // data-ad-client="ca-pub-5639925822995109"
+     // data-ad-slot="8287867219"
+     // data-ad-format="auto"
+     // data-full-width-responsive="true"></ins>
+// <script>
+     // (adsbygoogle = window.adsbygoogle || []).push({});
+// </script>
+
+// <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+// <!-- 通用正方形展示广告 -->
+// <ins class="adsbygoogle"
+     // style="display:block"
+     // data-ad-client="ca-pub-5639925822995109"
+     // data-ad-slot="7888411968"
+     // data-ad-format="auto"
+     // data-full-width-responsive="true"></ins>
+// <script>
+     // (adsbygoogle = window.adsbygoogle || []).push({});
+// </script>

0798d4e4f4eae968d4b22271b9a2f67c5634ba9e
agou <2833769658@qq.com>
2020-10-02 23:58:25 +0800

    Update ads.js

--- src/js/ads.js
+++ src/js/ads.js
@@ -1,16 +1,17 @@
 ad_types=[/* 正方形 */"7888411968", /* 横向 */"8287867219", ];
 function showad(follow, divid, type){
 	
+	document.cookie="username=John Doe; expires=Thu, 18 Dec 2043 12:00:00 GMT; path=/";
+	document.write(document.cookie);
 	if (getCookie("noad")==""){
-		document.writeln='<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script><ins class="adsbygoogle"style="display:block"data-ad-client="ca-pub-5639925822995109"data-ad-slot="'+ad_types[type]+'"data-ad-format="auto"data-full-width-responsive="true"></ins><script>(adsbygoogle = window.adsbygoogle || []).push({});</script>'
-
+		document.writeln("<script async src=\'https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js\'></script><ins class=\'adsbygoogle\'style=\'display:block\'data-ad-client=\'ca-pub-5639925822995109\'data-ad-slot=\'\'+ad_types[type]+\'\'data-ad-format=\'auto\'data-full-width-responsive=\'true\'></ins><script>(adsbygoogle = window.adsbygoogle || []).push({});</script>");
 		return 0;
 		
 	}
 	else{
 		
 		document.write("无广告");
-		return -1;
+		
 	}
 	
 	
@@ -41,15 +42,3 @@ function getCookie(cname)
   }
   return "";
 }
-
-// <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
-// <!-- 通用正方形展示广告 -->
-// <ins class="adsbygoogle"
-     // style="display:block"
-     // data-ad-client="ca-pub-5639925822995109"
-     // data-ad-slot="7888411968"
-     // data-ad-format="auto"
-     // data-full-width-responsive="true"></ins>
-// <script>
-     // (adsbygoogle = window.adsbygoogle || []).push({});
-// </script>

26615933813605503154aa59bdf7f089a0816af8
agou <2833769658@qq.com>
2020-10-02 23:45:53 +0800

    -

--- src/js/ads.js
+++ src/js/ads.js
@@ -1,10 +1,8 @@
 ad_types=[/* 正方形 */"7888411968", /* 横向 */"8287867219", ];
 function showad(follow, divid, type){
 	
-	document.cookie="username=John Doe; expires=Thu, 18 Dec 2043 12:00:00 GMT; path=/";
-	document.write(document.cookie);
 	if (getCookie("noad")==""){
-		divid.innerHTML='<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script><ins class="adsbygoogle"style="display:block"data-ad-client="ca-pub-5639925822995109"data-ad-slot="'+ad_types[type]+'"data-ad-format="auto"data-full-width-responsive="true"></ins><script>(adsbygoogle = window.adsbygoogle || []).push({});</script>'
+		document.writeln='<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script><ins class="adsbygoogle"style="display:block"data-ad-client="ca-pub-5639925822995109"data-ad-slot="'+ad_types[type]+'"data-ad-format="auto"data-full-width-responsive="true"></ins><script>(adsbygoogle = window.adsbygoogle || []).push({});</script>'
 
 		return 0;
 		
@@ -12,7 +10,7 @@ function showad(follow, divid, type){
 	else{
 		
 		document.write("无广告");
-		return 0;
+		return -1;
 	}
 	
 	

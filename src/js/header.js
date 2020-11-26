09df59e4b7c561e384d164328d1b97518d3d73e7
agou <2833769658@qq.com>
2020-11-27 00:09:14 +0800

    -

--- src/js/header.js
+++ src/js/header.js
@@ -10,32 +10,31 @@ c=loadc("/src/template/header.html");
 hcont.innerHTML=c;
 c=loadc("/src/template/footer.html");
 fcont.innerHTML=c;
-			var sit=0;
-			function go() {
-				if(sit==0){
-					document.getElementById("map-box").style.visibility = "visible";
-					document.getElementById("cover").style.visibility = "visible";
-					sit=1;
-				}
-				else{
-					document.getElementById("map-box").style.visibility = "hidden";
-					document.getElementById("cover").style.visibility = "hidden";
-					sit=0;
-				}
-			}
-			function stop(){
-				document.getElementById("map-box").style.visibility = "hidden";
-				document.getElementById("cover").style.visibility = "hidden";
-				sit=0;
-			}
-			window.onresize=function() {
-				if (document.body.clientWidth > 700)
-					document.getElementById("map-box").style.visibility = "visible";
-				else {
-					document.getElementById("map-box").style.visibility = "hidden";
-					document.getElementById("cover").style.visibility = "hidden";
+var sit=0;
+function go() {
+	if(sit==0){
+		document.getElementById("map-box").className = "map-box-opened";
+		cover.className = "cover-opened";
+		sit=1;
+	}
+	else{
+		document.getElementById("map-box").className = "";
+		cover.className = "";
+		sit=0;
+	}
+}
+function stop(){
+	document.getElementById("map-box").className = "";
+	cover.className = "";
+	sit=0;
+}
+// window.onresize=function() {
+	// if (document.body.clientWidth > 700)
+		// document.getElementById("map-box").style.visibility = "visible";
+	// else {
+		// document.getElementById("map-box").style.visibility = "hidden";
+		// document.getElementById("cover").style.visibility = "hidden";
 
-				}
-					
-			}
-			
+	// }
+		
+// }
